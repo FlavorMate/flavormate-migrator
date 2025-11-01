b626e1253a2d208ba69de8b794200184521df765
Chip3211 <github@thenus.de>
2025-11-01 12:42:31 +0100

    - fixed formatting

--- src/main/kotlin/de/flavormate/services/RecipeIngredientGroupItemNutritionService.kt
+++ src/main/kotlin/de/flavormate/services/RecipeIngredientGroupItemNutritionService.kt
@@ -14,50 +14,50 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 object RecipeIngredientGroupItemNutritionService {
 
-    fun migrate() {
-        val count = transaction(SourceDbInstance.getInstance()) { V2Nutrition.selectAll().count() }
+  fun migrate() {
+    val count = transaction(SourceDbInstance.getInstance()) { V2Nutrition.selectAll().count() }
 
-        for (i in 0..count step BATCH_SIZE) {
-            println("Migrating recipe ingredient group item nutrition ($i..${i + BATCH_SIZE}/$count)")
+    for (i in 0..count step BATCH_SIZE) {
+      println("Migrating recipe ingredient group item nutrition ($i..${i + BATCH_SIZE}/$count)")
 
-            val nutrition =
-                transaction(SourceDbInstance.getInstance()) {
-                    V2Nutrition.selectAll().offset(i).limit(BATCH_SIZE.toInt()).map { it }
-                }
-
-            transaction(TargetDbInstance.getInstance()) {
-                val offProducts =
-                    nutrition
-                        .filter { it[V2Nutrition.openFoodFactsId] != null }
-                        .map { it[V2Nutrition.openFoodFactsId]!! }
-                V3OpenFoodFactsProduct.batchInsert(offProducts) { product ->
-                    this[V3OpenFoodFactsProduct.id] = product.toString()
-                }
-            }
-
-            transaction(TargetDbInstance.getInstance()) {
-                V3RecipeIngredientGroupItemNutrition.batchInsert(nutrition) { nutrition ->
-                    val id =
-                        DynamicMappingTable.getOrCreateId(
-                            nutrition[V2Nutrition.id],
-                            DynamicMappingTable.recipeNutrition,
-                        )
-                    this[V3RecipeIngredientGroupItemNutrition.id] = id
-                    this[V3RecipeIngredientGroupItemNutrition.openFoodFactsId] =
-                        nutrition[V2Nutrition.openFoodFactsId]?.toString()
-                    this[V3RecipeIngredientGroupItemNutrition.carbohydrates] =
-                        nutrition[V2Nutrition.carbohydrates]
-                    this[V3RecipeIngredientGroupItemNutrition.energyKcal] = nutrition[V2Nutrition.energyKcal]
-                    this[V3RecipeIngredientGroupItemNutrition.fat] = nutrition[V2Nutrition.fat]
-                    this[V3RecipeIngredientGroupItemNutrition.saturatedFat] =
-                        nutrition[V2Nutrition.saturatedFat]
-                    this[V3RecipeIngredientGroupItemNutrition.sugars] = nutrition[V2Nutrition.sugars]
-                    this[V3RecipeIngredientGroupItemNutrition.fiber] = nutrition[V2Nutrition.fiber]
-                    this[V3RecipeIngredientGroupItemNutrition.proteins] = nutrition[V2Nutrition.proteins]
-                    this[V3RecipeIngredientGroupItemNutrition.salt] = nutrition[V2Nutrition.salt]
-                    this[V3RecipeIngredientGroupItemNutrition.sodium] = nutrition[V2Nutrition.sodium]
-                }
-            }
+      val nutrition =
+        transaction(SourceDbInstance.getInstance()) {
+          V2Nutrition.selectAll().offset(i).limit(BATCH_SIZE.toInt()).map { it }
         }
+
+      transaction(TargetDbInstance.getInstance()) {
+        val offProducts =
+          nutrition
+            .filter { it[V2Nutrition.openFoodFactsId] != null }
+            .map { it[V2Nutrition.openFoodFactsId]!! }
+        V3OpenFoodFactsProduct.batchInsert(offProducts) { product ->
+          this[V3OpenFoodFactsProduct.id] = product.toString()
+        }
+      }
+
+      transaction(TargetDbInstance.getInstance()) {
+        V3RecipeIngredientGroupItemNutrition.batchInsert(nutrition) { nutrition ->
+          val id =
+            DynamicMappingTable.getOrCreateId(
+              nutrition[V2Nutrition.id],
+              DynamicMappingTable.recipeNutrition,
+            )
+          this[V3RecipeIngredientGroupItemNutrition.id] = id
+          this[V3RecipeIngredientGroupItemNutrition.openFoodFactsId] =
+            nutrition[V2Nutrition.openFoodFactsId]?.toString()
+          this[V3RecipeIngredientGroupItemNutrition.carbohydrates] =
+            nutrition[V2Nutrition.carbohydrates]
+          this[V3RecipeIngredientGroupItemNutrition.energyKcal] = nutrition[V2Nutrition.energyKcal]
+          this[V3RecipeIngredientGroupItemNutrition.fat] = nutrition[V2Nutrition.fat]
+          this[V3RecipeIngredientGroupItemNutrition.saturatedFat] =
+            nutrition[V2Nutrition.saturatedFat]
+          this[V3RecipeIngredientGroupItemNutrition.sugars] = nutrition[V2Nutrition.sugars]
+          this[V3RecipeIngredientGroupItemNutrition.fiber] = nutrition[V2Nutrition.fiber]
+          this[V3RecipeIngredientGroupItemNutrition.proteins] = nutrition[V2Nutrition.proteins]
+          this[V3RecipeIngredientGroupItemNutrition.salt] = nutrition[V2Nutrition.salt]
+          this[V3RecipeIngredientGroupItemNutrition.sodium] = nutrition[V2Nutrition.sodium]
+        }
+      }
     }
+  }
 }
